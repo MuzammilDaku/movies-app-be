dd9dd7cecdf462b45c27867f6199d3b84223fb0e
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2023-06-25 17:24:06 +0500

    cluster added

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`).then(()=>console.log('database connected')).catch((e)=>{
+mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true,
+    useUnifiedTopology: true,}).then(()=>console.log('database connected')).catch((e)=>{
     console.log(`data base error` ,e)
 })
